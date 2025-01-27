c5c33c37a0dafe8e16bc54a6712f9650594d9728
Robert Runge <roru@clever.dk>
2025-01-27 14:10:34 +0100

    feature: _00 added logging to program.cs with loggerFacory

--- README.md
+++ README.md
@@ -1,4 +1,13 @@
 # dotnet-logging
 
-## WebApiLocalLogging
+## 00 Console app logging to console
+Shows how use LoggerFactory to control LogLevel and log using ILogger. All logging is to console.
+
+## 01 WebApiLocalLogging
 Logging to console using `Microsoft.Extensions.Logging` in a WebApi project.
+
+## 02 Web Api logging using serilog
+
+## 03 Web Api logging to app insights (no serilog)
+
+## 04 Web Api logging to app insights using serilog
---
+++ _00-VeryBasicConsoleAppLogging/Program.cs
@@ -0,0 +1,46 @@
+﻿using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
+using Microsoft.Extensions.Logging;
+
+var builder = Host.CreateApplicationBuilder(args);
+
+// The line below controls the loggerAfter but not the loggerBefore or anotherLoggerBefore
+builder.Logging.AddConsole()
+    //.SetMinimumLevel(LogLevel.Warning)
+    ;
+
+using var loggerFactory = LoggerFactory.Create(loggingBuilder =>
+{
+    loggingBuilder
+        .AddConsole()
+        .SetMinimumLevel(LogLevel.Debug);
+});
+
+using var anotherLoggerFactory = LoggerFactory.Create(loggingBuilder =>
+{
+    loggingBuilder
+        .AddConsole()
+        .SetMinimumLevel(LogLevel.Trace);
+});
+
+var loggerBefore = loggerFactory.CreateLogger<Program>();
+loggerBefore.LogTrace("LogTrace before host build (should not be shown)");
+loggerBefore.LogDebug("LogDebug before host build");
+loggerBefore.LogInformation("LogInformation before host build");
+
+var anotherLoggerBefore = anotherLoggerFactory.CreateLogger<Program>();
+anotherLoggerBefore.LogTrace("Another LogTrace before host build");
+anotherLoggerBefore.LogDebug("Another LogDebug before host build");
+anotherLoggerBefore.LogInformation("Another LogInformation before host build");
+
+using IHost host = builder.Build();
+
+var loggerAfter = host.Services.GetRequiredService<ILogger<Program>>();
+loggerAfter.LogTrace("LogTrace right after host build (should not be shown)");
+loggerAfter.LogDebug("LogDebug right after host build (should not be shown)");
+loggerAfter.LogInformation("LogInformation right after host build");
+
+Console.WriteLine("Hello, World!");
+
+
+await host.RunAsync();
