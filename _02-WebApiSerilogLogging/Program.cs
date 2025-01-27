2b713bbd55ed5e0d4f598757c2a37297d0449fd9
Robert Runge <roru@clever.dk>
2025-01-27 20:00:11 +0100

    feature: 03 added  serilog in a web api

---
+++ _02-WebApiSerilogLogging/Program.cs
@@ -0,0 +1,37 @@
+using CommonLogging;
+using Scalar.AspNetCore;
+using Serilog;
+using Log = Serilog.Log;
+
+var builder = WebApplication.CreateBuilder(args);
+
+// Log.Logger = new LoggerConfiguration()
+//     .MinimumLevel.Debug()
+//     .WriteTo.Console()
+//     .WriteTo.File("logs/log.txt", rollingInterval: RollingInterval.Day)
+//     .CreateLogger();
+//
+// var msg = "Hello, World! " + DateTime.Now.ToString("u");
+// Log.Information(msg);
+
+builder.Services.AddOpenApi();
+
+builder.Host.UseSerilog((context, configuration) =>
+    configuration.ReadFrom.Configuration(context.Configuration));
+
+var app = builder.Build();
+app.UseSerilogRequestLogging();
+
+app.MapOpenApi();
+app.MapScalarApiReference(options => //  => https://localhost:7156/scalar/v1
+{
+    options.WithTitle(".WithTitle from Program.cs");
+});
+
+
+app.UseHttpsRedirection();
+
+app.AddLoggingApi();
+
+app.Run();
+
